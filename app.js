99f4fa8000566e3c9534c3360b30761c1876f7e1
ibntofajjal <joynal@bintofajjal.info>
2021-08-18 11:14:06 +0600

    Typed Number and add condition for Clear and Del : Module 27 - 6 video

--- app.js
+++ app.js
@@ -6,10 +6,27 @@ function getPin () {
     } else {
         return getPin();
     }
-}
+};
 
 function generatePin() {
     const pin = getPin();
     document.getElementById("display-pin").value = pin;
     // console.log(pin);
-}
+};
+
+document.getElementById("key-pad").addEventListener("click", function(event){
+    const number = event.target.innerText;
+    const calcInput = document.getElementById("typed-numbers");
+    if(isNaN(number)){
+        if(number == 'C') {
+            calcInput.value = '';
+        }
+        if (number == '🔪') {
+            calcInput.value = calcInput.value.slice(0,-1);
+        }
+    } else {
+        const previousNumber = calcInput.value;
+        const newNumber = previousNumber + number;
+        calcInput.value = newNumber;
+    }
+});
